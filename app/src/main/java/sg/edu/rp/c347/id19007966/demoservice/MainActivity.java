0e146f226dc6ff259c7f1aa3fe67e432e27814f0
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-07-27 10:40:59 +0800

    Section B codes

--- app/src/main/java/sg/edu/rp/c347/id19007966/demoservice/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoservice/MainActivity.java
@@ -2,13 +2,32 @@ package sg.edu.rp.c347.id19007966.demoservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonStart, buttonStop;
+    Button buttonStart, buttonStop, buttonBind, buttonUnbind;
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder) iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         buttonStart = findViewById(R.id.buttonStart);
         buttonStop = findViewById(R.id.buttonStop);
+        buttonBind = findViewById(R.id.buttonBind);
+        buttonUnbind = findViewById(R.id.buttonUnbind);
 
         buttonStart.setOnClickListener(view -> {
             Intent i = new Intent(MainActivity.this, MyService.class);
@@ -25,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
         buttonStop.setOnClickListener(view -> {
             Intent i = new Intent(MainActivity.this, MyService.class);
             stopService(i);
+        });
+
+        buttonBind.setOnClickListener(view -> {
+            Intent bindIntent = new Intent(MainActivity.this, MyService.class);
+            bindService(bindIntent, connection, BIND_AUTO_CREATE);
+        });
+
+        buttonUnbind.setOnClickListener(view -> {
+            unbindService(connection);
         });
     }
 }
--- app/src/main/java/sg/edu/rp/c347/id19007966/demoservice/MyService.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoservice/MyService.java
@@ -2,6 +2,7 @@ package sg.edu.rp.c347.id19007966.demoservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -9,10 +10,24 @@ public class MyService extends Service {
 
     boolean started;
 
+    private DownloadBinder downloadBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload() {
+            Log.d("MyService", "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress executed");
+            return 0;
+        }
+
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return downloadBinder;
     }
 
     @Override
