00924830045e8136f20aa7b9feabc5c4e90b1ec6
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-07-27 10:18:10 +0800

    Section A codes

--- app/src/main/java/sg/edu/rp/c347/id19007966/demoservice/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoservice/MainActivity.java
@@ -2,13 +2,29 @@ package sg.edu.rp.c347.id19007966.demoservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonStart, buttonStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+
+        buttonStart.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, MyService.class);
+            startService(i);
+        });
+
+        buttonStop.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, MyService.class);
+            stopService(i);
+        });
     }
 }
---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoservice/MyService.java
@@ -0,0 +1,41 @@
+package sg.edu.rp.c347.id19007966.demoservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    boolean started;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d("MyService", "Service created");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (started == false){
+            started = true;
+            Log.d("MyService", "Service started");
+        } else {
+            Log.d("MyService", "Service is still running");
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("MyService", "Service exited");
+        super.onDestroy();
+    }
+
+}
